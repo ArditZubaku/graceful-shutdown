f43813b8adf010e079ed56c6b96340efc86b729f
ArditZubaku <zubaku92@gmail.com>
2025-11-10 16:19:16 +0100

    Gracefully shutting down the HTTP server

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -11,8 +15,33 @@ func main() {
 		Handler: routes(),
 	}
 
-	fmt.Println("Listening on :8000")
-	if err := s.ListenAndServe(); err != nil {
-		fmt.Printf("Stopped listening: %v\n", err)
+	go func() {
+		fmt.Println("Listening on :8000")
+		if err := s.ListenAndServe(); err != nil {
+			fmt.Printf("Stopped listening: %v\n", err)
+		}
+	}()
+
+	shutdown, stop := signal.NotifyContext(
+		context.Background(),
+		syscall.SIGTERM,
+		syscall.SIGINT,
+	)
+	defer stop()
+
+	// We are blocking until we receive a shutdown signal (CTRL+C for example)
+	<-shutdown.Done()
+
+	fmt.Println("Shutting server down...")
+	// We give the server X seconds to shutdown gracefully or we do it forcefully
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	// This starts the shutdown process, waits until all the handlers finish and then kills the server
+	// Stops accepting new connections when in shutdown mode.
+	if err := s.Shutdown(ctx); err != nil {
+		fmt.Printf("Shut down with error: %v", err)
+	} else {
+		fmt.Println("Successfully shut down the server!")
 	}
 }
